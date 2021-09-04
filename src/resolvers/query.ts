c61dca03adc6e5eae72c45b8712958c676c0dde2
burbano444 <burbano444@hotmail.com>
2021-09-04 11:40:49 -0500

    first commit

---
+++ src/resolvers/query.ts
@@ -0,0 +1,13 @@
+import { IResolvers } from "@graphql-tools/utils";
+import { database } from "../data/data.store";
+ 
+const query: IResolvers = {
+       Query: {
+           estudiantes(): any {
+               return database.estudiantes;
+           }
+           
+       }
+}
+ 
+export default query;
